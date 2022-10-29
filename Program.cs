d522105adbf1eb37c0dfe42da14b788b7a696760
Rajashekar Vemula <vemula.rajashekar02@gmail.com>
2022-10-29 20:40:25 +0530

    added create a stack problem program

--- Program.cs
+++ Program.cs
@@ -4,7 +4,11 @@
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Stacks and Queues Examples");             
+            LinkedList stack = new LinkedList();
+            stack.Push(56);
+            stack.Push(30);
+            stack.Push(70);
+            stack.Display();
         }
     }
 }
